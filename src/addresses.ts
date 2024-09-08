31bdf4e0c1b97b58f9383f577288ae7792006906
hippocampusWeb3 <hippocampus.web3@proton.me>
2024-09-08 19:30:47 +0200

    Fix build

--- src/addresses.ts
+++ src/addresses.ts
@@ -32,7 +32,8 @@ export const getAddresses = (network: string): Addresses => {
     case "base":
       return {
         UniswapV3Factory: "0x33128a8fC17869897dcE68Ed026d694621f6FDfD",
-        SwapRouter: "0x2626664c2603336E57B271c5C0b26F421741e481",
+        SwapRouter: "",
+        SwapRouter02: "0x2626664c2603336E57B271c5C0b26F421741e481",
         WETH: "0x4200000000000000000000000000000000000006",
         WMATIC: "",
         USDC: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
